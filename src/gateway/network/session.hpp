5dee046f80e5ecac65ee68308281413352b69fc3
brokeboi2006 <3191212@mail.ru>
2026-04-23 22:49:41 +0300

    добавил .gitignore и еще раз переделал структуру проекта, которой буду придерживаться до конца

--- src/gateway/network/session.hpp
+++ src/gateway/network/session.hpp
@@ -30,45 +30,14 @@ class Session : public std::enable_shared_from_this<Session> {
                 [this, self](beast::error_code ec, std::size_t length) {
                     boost::ignore_unused(length);
                     if (!ec) {
-                        process_request();
+                        // process_request();
                     }
                 } 
             );
             
         }
-        void do_write_dummy_response() {
-            auto self = shared_from_this();
-    
-            // Создаем простейший ответ 200 OK
-            auto res = std::make_shared<http::response<http::string_body>>(http::status::ok, req_.version());
-            res->set(http::field::server, "My-C++-Gateway");
-            res->body() = "Request received!";
-            res->prepare_payload(); // Важно: Beast сам рассчитает Content-Length
-
-            http::async_write(
-                socket_, 
-                *res,
-                [this, self, res](beast::error_code ec, std::size_t) {
-                    socket_.shutdown(tcp::socket::shutdown_send, ec); // Закрываем запись
-            });
-        }
-        void process_request() {
-            std::cout << "---- NEW REQUEST RECEIVED ----" << std::endl;
-            std::cout << "request-method : " << req_.method_string() << std::endl;
-            std::cout << "target string  : " << req_.target() << std::endl;
-
-            std::cout << "-- HEADERS -- " << std::endl;
-            for(const auto& field : req_) {
-                std::cout << "  " << field.name_string() << " : " << field.value() << std::endl;
-            }
-
-            if(!req_.body().empty()) {
-                std::cout << "Body: " << req_.body() << std::endl;
-            }
-            do_write_dummy_response();
-        }
-        private:
-            tcp::socket socket_;
-            beast::flat_buffer buffer_; 
-            http::request<http::string_body> req_;
+    private:
+        tcp::socket socket_;
+        beast::flat_buffer buffer_; 
+        http::request<http::string_body> req_;
 };
